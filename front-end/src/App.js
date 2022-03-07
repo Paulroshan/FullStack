805d4f28dc38a9d53c1554a5681a7eaf59aa6f2b
Paulroshan <paulroshan6193@gmail.com>
2022-03-07 13:00:29 +0530

    five

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MD } from "./First";
+
+export const Design=()=>{
+
+    const ds={
+        color: 'red'
+    }
+    return(
+        <>
+        <p style={ds}>PAUL</p>
+
+        <MD/>
+        </>
+        
+    )
+}
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Design } from './App';
+import { Sasi } from './Design';
+import { MD } from './First';
 
 ReactDOM.render(
   <>
   <h1>hello man</h1>
+  <Design/>
+  <MD/>
+  <Sasi/>
   </>
   ,document.getElementById('root')
 );
